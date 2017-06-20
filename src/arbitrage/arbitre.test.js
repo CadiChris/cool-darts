7f28da9fbc16c06c1ddcc5cbf15aa6a3d7a12e91
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-20 13:51:35 +0200

    Utilisation de toMatchSnapshot Je ne suis pas convaincu que cela améliore le code, mais j'ai envie d'essayer. Je trouve que le snapshot rend plus difficile la lecture de l'attendu. Mais en contrepartie on a un modèle exprimé totalement dans les fichiers .snap. À voir sur le long terme ce qui est préférable.

--- src/arbitrage/arbitre.test.js
+++ src/arbitrage/arbitre.test.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 import assert from "assert";
 import freeze from "deep-freeze";
 import {scoreVierge} from "./score";
@@ -27,79 +27,74 @@ const _1_TOUCHE = 1
 const _2_TOUCHES = 2
 const _3_TOUCHES = 3
 
-describe('arbitre', () => {
-  describe('#calculerLeNouveauScore', () => {
-    it('Ferme un chiffre après 3 touches', () => {
-      let nouveauxScores = arbitre.calculerLeNouveauScore(
-        [
-          score('J1', {20: _2_TOUCHES}, 0)
-        ],
-        lancerDansLe(20, 'J1'));
+describe('#calculerLeNouveauScore', () => {
+  it('Ferme un chiffre après 3 touches', () => {
+    let nouveauxScores = arbitre.calculerLeNouveauScore(
+      [
+        score('J1', {20: _2_TOUCHES}, 0)
+      ],
+      lancerDansLe(20, 'J1'))
 
-      assert.deepEqual(nouveauxScores[0].cible, cible({20: _3_TOUCHES}));
-    });
+    expect(nouveauxScores[0].cible).toMatchSnapshot()
+  })
 
-    it("N'augmente pas les touches d'un chiffre fermé", () => {
-      let nouveauxScores = arbitre.calculerLeNouveauScore(
-        [
-          score('J1', {20: _3_TOUCHES}, 0)
-        ],
-        lancerDansLe(20, 'J1'));
+  it("N'augmente pas les touches d'un chiffre fermé", () => {
+    let nouveauxScores = arbitre.calculerLeNouveauScore(
+      [
+        score('J1', {20: _3_TOUCHES}, 0)
+      ],
+      lancerDansLe(20, 'J1'))
 
-      assert.deepEqual(nouveauxScores[0].cible, cible({20: _3_TOUCHES}));
-    });
+    expect(nouveauxScores[0].cible).toMatchSnapshot()
+  })
 
-    it('Ne pénalise pas les adversaires si le lancer ne ferme pas le chiffre', () => {
-      let nouveauxScores = arbitre.calculerLeNouveauScore(
-        [
-          score('J1', {20: _0_TOUCHE}, 0),
-          score('J2', {20: _0_TOUCHE}, 0)
-        ],
-        lancerDansLe(20, 'J1'));
-
-      assert.deepEqual(nouveauxScores, [
-        score('J1', {20: _1_TOUCHE}, 0),
+  it('Ne pénalise pas les adversaires si le lancer ne ferme pas le chiffre', () => {
+    let nouveauxScores = arbitre.calculerLeNouveauScore(
+      [
+        score('J1', {20: _0_TOUCHE}, 0),
         score('J2', {20: _0_TOUCHE}, 0)
-      ]);
-    });
+      ],
+      lancerDansLe(20, 'J1'));
 
-    it('Marque des points aux adversaires ouverts', () => {
-      let nouveauxScores = arbitre.calculerLeNouveauScore(
-        [
-          score('J1', {20: _3_TOUCHES}, 0),
-          score('J2', {20: _0_TOUCHE}, 10)
-        ],
-        lancerDansLe(20, 'J1'));
+    expect(nouveauxScores).toMatchSnapshot()
+  })
 
-      assert.strictEqual(nouveauxScores[1].points, 30);
-    });
+  it('Marque des points aux adversaires ouverts', () => {
+    let nouveauxScores = arbitre.calculerLeNouveauScore(
+      [
+        score('J1', {20: _3_TOUCHES}, 0),
+        score('J2', {20: _0_TOUCHE}, 10)
+      ],
+      lancerDansLe(20, 'J1'));
 
-    it('Ne marque pas de points aux adversaires fermés', () => {
-      let nouveauxScores = arbitre.calculerLeNouveauScore(
-        [
-          score('J1', {20: _3_TOUCHES}, 0),
-          score('J2', {20: _3_TOUCHES}, 0)
-        ],
-        lancerDansLe(20, 'J1'));
-
-      assert.strictEqual(nouveauxScores[1].points, 0);
-    });
-
-    it("Préserve l'ordre des scores", () => {
-      let nouveauxScores = arbitre.calculerLeNouveauScore(
-        [
-          score('J1', {20: _0_TOUCHE}, 0),
-          score('J2', {20: _0_TOUCHE}, 0),
-          score('J3', {20: _0_TOUCHE}, 0)
-        ],
-        lancerDansLe(20, 'J2'));
-
-      assert.strictEqual(nouveauxScores[0].joueur, 'J1');
-      assert.strictEqual(nouveauxScores[1].joueur, 'J2');
-      assert.strictEqual(nouveauxScores[2].joueur, 'J3');
-    });
+    assert.strictEqual(nouveauxScores[1].points, 30);
   });
-});
+
+  it('Ne marque pas de points aux adversaires fermés', () => {
+    let nouveauxScores = arbitre.calculerLeNouveauScore(
+      [
+        score('J1', {20: _3_TOUCHES}, 0),
+        score('J2', {20: _3_TOUCHES}, 0)
+      ],
+      lancerDansLe(20, 'J1'));
+
+    assert.strictEqual(nouveauxScores[1].points, 0);
+  });
+
+  it("Préserve l'ordre des scores", () => {
+    let nouveauxScores = arbitre.calculerLeNouveauScore(
+      [
+        score('J1', {20: _0_TOUCHE}, 0),
+        score('J2', {20: _0_TOUCHE}, 0),
+        score('J3', {20: _0_TOUCHE}, 0)
+      ],
+      lancerDansLe(20, 'J2'));
+
+    assert.strictEqual(nouveauxScores[0].joueur, 'J1');
+    assert.strictEqual(nouveauxScores[1].joueur, 'J2');
+    assert.strictEqual(nouveauxScores[2].joueur, 'J3');
+  })
+})
 
 function score(leJoueur, sesChiffres, sesPoints) {
   let score = Object.assign({}, scoreVierge(leJoueur), {
--- src/arbitrage/score.test.js
+++ src/arbitrage/score.test.js
@@ -4,21 +4,7 @@ import {scoreVierge} from "./score";
 
 describe('score.modele', () => {
   it('crée un score vierge', () => {
-    let scoreViergeJ1 = scoreVierge('J1')
-
-    assert.deepEqual(scoreViergeJ1, {
-      joueur: 'J1',
-      points: 0,
-      cible: {
-        15: {touches: 0, ferme: false},
-        16: {touches: 0, ferme: false},
-        17: {touches: 0, ferme: false},
-        18: {touches: 0, ferme: false},
-        19: {touches: 0, ferme: false},
-        20: {touches: 0, ferme: false},
-        25: {touches: 0, ferme: false}
-      }
-    })
+    expect(scoreVierge('J1')).toMatchSnapshot()
   })
 
   it('créé un nouveau score à chaque appel', () => {
