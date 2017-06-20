525e3864567fd98e658013f55641f4e1857b0683
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-20 12:04:17 +0200

    Suppression des ';'. Cela améliore la lisibilité.

--- src/score/arbitre.js
+++ src/score/arbitre.js
@@ -1,43 +1,44 @@
-'use strict';
+'use strict'
+
 import {toucher, penaliser, chiffreEstFerme} from "./score";
 
 export function calculerLeNouveauScore(tableauDesScores, lancer) {
-  let penalite = penaliteDuLancer(tableauDesScores, lancer);
+  let penalite = penaliteDuLancer(tableauDesScores, lancer)
 
   return tableauDesScores.map(s => {
     return scorer(s, lancer, penalite)
   });
 }
 
-const LIMITE = 3;
+const LIMITE = 3
 
 function penaliteDuLancer(scores, lancer) {
-  let scoreDuLanceur = trouverLeScoreDuLanceur(scores, lancer.lanceur);
-  return calculerLaPenalite(scoreDuLanceur.cible, lancer);
+  let scoreDuLanceur = trouverLeScoreDuLanceur(scores, lancer.lanceur)
+  return calculerLaPenalite(scoreDuLanceur.cible, lancer)
 }
 
 function trouverLeScoreDuLanceur(scores, lanceur) {
   return scores.filter(s => {
     return s.joueur === lanceur
-  })[0];
+  })[0]
 }
 
 function calculerLaPenalite(cibleDuLanceur, lancer) {
-  let touches = cibleDuLanceur[lancer.chiffre].touches;
-  let leChiffreVaSeFermer = touches + lancer.touches >= LIMITE;
-  let surplus = (touches + lancer.touches) - LIMITE;
+  let touches = cibleDuLanceur[lancer.chiffre].touches
+  let leChiffreVaSeFermer = touches + lancer.touches >= LIMITE
+  let surplus = (touches + lancer.touches) - LIMITE
 
-  return leChiffreVaSeFermer ? surplus * lancer.chiffre : 0;
+  return leChiffreVaSeFermer ? surplus * lancer.chiffre : 0
 }
 
 function scorer(score, lancer, penalite) {
   return score.joueur === lancer.lanceur
     ? toucher(lancer.chiffre, lancer.touches, score)
-    : scorerUnAdversaire(score, lancer.chiffre, penalite);
+    : scorerUnAdversaire(score, lancer.chiffre, penalite)
 }
 
 function scorerUnAdversaire(score, chiffre, pointsDePenalite) {
   return chiffreEstFerme(chiffre, score)
     ? score
-    : penaliser(pointsDePenalite, score);
+    : penaliser(pointsDePenalite, score)
 }
