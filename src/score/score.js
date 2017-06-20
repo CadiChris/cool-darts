14f63996a15a1b50095f0a020857c66fe36c6d52
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-20 10:56:41 +0200

    [Boyscout] Format

--- src/score/score.js
+++ src/score/score.js
@@ -1,43 +1,43 @@
 'use strict';
 
 export function scoreVierge(joueur) {
-	return {
-		joueur: joueur,
-		points: 0,
-		cible: {
-			15: { touches: 0, ferme: false},
-			16: { touches: 0, ferme: false},
-			17: { touches: 0, ferme: false},
-			18: { touches: 0, ferme: false},
-			19: { touches: 0, ferme: false},
-			20: { touches: 0, ferme: false},
-			25: { touches: 0, ferme: false}
-		}
-	};
+  return {
+    joueur: joueur,
+    points: 0,
+    cible: {
+      15: {touches: 0, ferme: false},
+      16: {touches: 0, ferme: false},
+      17: {touches: 0, ferme: false},
+      18: {touches: 0, ferme: false},
+      19: {touches: 0, ferme: false},
+      20: {touches: 0, ferme: false},
+      25: {touches: 0, ferme: false}
+    }
+  };
 }
 
 const LIMITE = 3;
 
 export function toucher(chiffre, touches, score) {
-	const nouvellesTouches = Math.min(score.cible[chiffre].touches + touches, LIMITE);
-	
-	return Object.assign({},
-		score, {
-			cible: Object.assign({}, score.cible, {
-				[chiffre]: {
-					touches: nouvellesTouches,
-					ferme: nouvellesTouches >= LIMITE
-			}
-		})
-	});
+  const nouvellesTouches = Math.min(score.cible[chiffre].touches + touches, LIMITE);
+
+  return Object.assign({},
+    score, {
+      cible: Object.assign({}, score.cible, {
+        [chiffre]: {
+          touches: nouvellesTouches,
+          ferme: nouvellesTouches >= LIMITE
+        }
+      })
+    });
 }
 
 export function penaliser(pointsDePenalite, score) {
-	return Object.assign({}, score, {
-		points: score.points + pointsDePenalite
-	});
+  return Object.assign({}, score, {
+    points: score.points + pointsDePenalite
+  });
 }
 
 export function chiffreEstFerme(chiffre, score) {
-	return score.cible[chiffre].ferme;
+  return score.cible[chiffre].ferme;
 }
