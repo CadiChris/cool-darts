4b79bee5fcef1ceb11665cf242f9b0e3c6009399
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-23 23:00:39 +0200

    Inscription des joueurs avec leur nom.

--- app-react-native/cricket/partie/InscriptionDesJoueurs.js
+++ app-react-native/cricket/partie/InscriptionDesJoueurs.js
@@ -1,25 +1,53 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View, Button } from 'react-native'
-import { inscrireJoueur } from './Partie.actions'
+import { View, Button, TextInput } from 'react-native'
+import { inscrireJoueur, demarrerPartie } from './Partie.actions'
 
 class InscriptionDesJoueurs extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      joueur: ''
+    }
+  }
+
+  render() {
+    const {peutDemarrer} = this.props
+    const {joueur} = this.state
+    return (
+      <View style={{height: 100}}>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <TextInput
+            value={joueur}
+            onChangeText={(text) => this.setState({joueur: text})}
+            style={{height: 40, width: 100}}
+            placeholder="Joueur"/>
+          <Button title="Nouveau joueur" onPress={() => this.ajouterJoueur()}/>
+        </View>
+        {
+          peutDemarrer ? <Button title="GO" onPress={() => this.demarrerLaPartie()}/> : null
+        }
+      </View>
+    )
   }
 
   ajouterJoueur() {
     const {dispatch} = this.props
-    dispatch(inscrireJoueur('J1'))
+    const {joueur}=this.state
+    dispatch(inscrireJoueur(joueur))
+    this.setState({joueur: ''})
   }
 
-  render() {
-    return (
-      <View>
-        <Button title="Nouveau joueur" onPress={() => this.ajouterJoueur()}/>
-      </View>
-    )
+  demarrerLaPartie() {
+    const {dispatch} = this.props
+    dispatch(demarrerPartie())
   }
 }
 
-export default connect()(InscriptionDesJoueurs)
+function mapStateToProps(state) {
+  return {
+    peutDemarrer: state.partie.peutDemarrer
+  }
+}
+
+export default connect(mapStateToProps)(InscriptionDesJoueurs)
