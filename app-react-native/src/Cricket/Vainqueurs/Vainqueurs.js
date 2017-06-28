d4f46a82fba965efb5b9ed7593f182a13e1fe978
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-28 22:45:56 +0200

    Bouton nouvelle partie sur l'écran des vainqueurs

--- app-react-native/src/Cricket/Vainqueurs/Vainqueurs.js
+++ app-react-native/src/Cricket/Vainqueurs/Vainqueurs.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Text, View } from 'react-native'
-import { Styles, Textes } from '../styles'
+import { Styles, Textes, Boutons } from '../styles'
+import Button from 'apsl-react-native-button'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { nouvellePartie } from '../Cricket.actions'
 
 class Vainqueurs extends React.Component {
 
@@ -12,20 +15,33 @@ class Vainqueurs extends React.Component {
   render() {
     const {vainqueurs} = this.props
     return (
-      <View style={[Styles.contenuAuMilieu, {justifyContent:'space-around'}]}>
-        <Text style={[Textes.titre, {}]} >Fin de partie</Text>
-        <View style={{height: '40%', }}>
-          <Text style={[Textes.light, {fontSize: 18, opacity: 0.8, textAlign:'center'}]}>Vainqueur</Text>
-          <Text style={[Textes.titre, {textAlign:'center'}]}> {vainqueurs.map(v => v)}</Text>
+      <View style={[Styles.contenuAuMilieu, {justifyContent: 'space-around'}]}>
+        <Text style={[Textes.titre, {}]}>Fin de partie</Text>
+        <View style={{height: '70%', justifyContent: 'space-around'}}>
+          <View>
+            <Text style={[Textes.light, {fontSize: 18, opacity: 0.8, textAlign: 'center'}]}>Vainqueur</Text>
+            <Text style={[Textes.titre, {textAlign: 'center'}]}> {vainqueurs.map(v => v)}</Text>
+          </View>
+          <Button
+            onPress={() => this.nouvellePartie()}
+            style={[Boutons.deCommande, {width: 60, height: 50, marginHorizontal: 30}]}
+            textStyle={[Textes.bouton, {fontSize: 12}]}>
+            <Icon name="home" size={20} color="white"/>
+          </Button>
         </View>
       </View>
     )
+  }
+
+  nouvellePartie() {
+    const {dispatch} = this.props
+    dispatch(nouvellePartie())
   }
 }
 
 function mapStateToProps(state) {
   return {
-    vainqueurs: state.cricket.vainqueurs
+    vainqueurs: state.cricket.actuel.vainqueurs
   }
 }
 
