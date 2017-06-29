e2be01c711481b62c60562a6d33fb312579f39e2
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-29 16:17:41 +0200

    [Refacto] Éclatement en 1 container + 1 vue pure.

---
+++ app-react-native/src/Cricket/Vainqueurs/Vainqueurs.container.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { nouvellePartie } from '../Cricket.actions'
+import Vainqueurs from './Vainqueurs'
+
+class VainqueursContainer extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <Vainqueurs {...this.props} />
+  }
+}
+
+const mapStateToProps = (state) => ({
+  vainqueurs: state.cricket.actuel.vainqueurs
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  declencherNouvellePartie: () => dispatch(nouvellePartie())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(VainqueursContainer)
--- app-react-native/src/Cricket/Vainqueurs/Vainqueurs.js
+++ app-react-native/src/Cricket/Vainqueurs/Vainqueurs.js
@@ -1,51 +1,32 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Text, View } from 'react-native'
 import { Styles, Textes, Boutons } from '../styles'
 import ViewQuiDecale from '../Technique/ViewQuiDecale'
 import Button from 'apsl-react-native-button'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import { nouvellePartie } from '../Cricket.actions'
 
-class Vainqueurs extends React.Component {
+export default ({vainqueurs, declencherNouvellePartie}) => (
+  <View dureeDuDecalage={1200} coteDeDepart="left" style={[Styles.contenuAuMilieu, {justifyContent: 'space-around'}]}>
 
-  constructor(props) {
-    super(props)
-  }
+    <ViewQuiDecale dureeDuDecalage={1200} coteDeDepart="right">
+      <Text style={[Textes.titre, {}]}>
+        Fin de partie
+      </Text>
+    </ViewQuiDecale>
 
-  render() {
-    const {vainqueurs} = this.props
-    return (
-      <View dureeDuDecalage={1200} coteDeDepart="left" style={[Styles.contenuAuMilieu, {justifyContent: 'space-around'}]}>
-        <ViewQuiDecale dureeDuDecalage={1200} coteDeDepart="right">
-          <Text style={[Textes.titre, {}]}>Fin de partie</Text>
-        </ViewQuiDecale>
-        <ViewQuiDecale dureeDuDecalage={1200} coteDeDepart="left" style={[{height: '70%', justifyContent: 'space-around'}]}>
-          <View>
-            <Text style={[Textes.light, {fontSize: 18, opacity: 0.8, textAlign: 'center'}]}>Vainqueur</Text>
-            <Text style={[Textes.titre, {textAlign: 'center'}]}> {vainqueurs.map(v => v)}</Text>
-          </View>
-          <Button
-            onPress={() => this.nouvellePartie()}
-            style={[Boutons.deCommande, {width: 60, height: 50, alignSelf:'center'}]}
-            textStyle={[Textes.bouton, {fontSize: 12}]}>
-            <Icon name="home" size={20} color="white"/>
-          </Button>
-        </ViewQuiDecale>
+    <ViewQuiDecale dureeDuDecalage={1200} coteDeDepart="left" style={[{height: '70%', justifyContent: 'space-around'}]}>
+      <View>
+        <Text style={[Textes.light, {fontSize: 18, opacity: 0.8, textAlign: 'center'}]}>Vainqueur</Text>
+        <Text style={[Textes.titre, {textAlign: 'center'}]}> {vainqueurs.map(v => v)}</Text>
       </View>
-    )
-  }
 
-  nouvellePartie() {
-    const {dispatch} = this.props
-    dispatch(nouvellePartie())
-  }
-}
+      <Button
+        onPress={declencherNouvellePartie}
+        style={[Boutons.deCommande, {width: 60, height: 50, alignSelf: 'center'}]}
+        textStyle={[Textes.bouton, {fontSize: 12}]}>
+        <Icon name="home" size={20} color="white"/>
+      </Button>
+    </ViewQuiDecale>
 
-function mapStateToProps(state) {
-  return {
-    vainqueurs: state.cricket.actuel.vainqueurs
-  }
-}
-
-export default connect(mapStateToProps)(Vainqueurs)
+  </View>
+)
