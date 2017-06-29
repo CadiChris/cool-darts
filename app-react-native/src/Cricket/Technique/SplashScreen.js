90b0386ad991b71b96340c7e7ecb94792a0f3f45
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-29 14:58:16 +0200

    Ajout du Splashscreen

---
+++ app-react-native/src/Cricket/Technique/SplashScreen.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { Styles, Textes } from '../styles'
+import FadeInView from './FadeInView'
+
+class SplashScreen extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      splashScreenActif: true
+    }
+  }
+
+  componentDidMount() {
+    const {dureeDuSplash} = this.props
+    setTimeout(() => { this.setState({splashScreenActif:false})}, dureeDuSplash)
+  }
+
+  render() {
+    const {splashScreenActif} = this.state
+    const {dureeDuSplash} = this.props
+
+    const splashScreen = (
+      <FadeInView dureeDuFade={dureeDuSplash / 1.5} style={[Styles.container, {backgroundColor: 'rgb(58,127,144)'}]}>
+        <View style={[{height:'75%', alignItems:'center', justifyContent:'center'}]}>
+          <Image source={require('./../../../assets/splashscreen.png')} style={{width:'66%'}} resizeMode='contain'/>
+          <Text style={[Textes.titre, {fontSize: 15, textAlign:'center'}]}>
+            Développeurs de produits sur mesure
+          </Text>
+        </View>
+      </FadeInView>
+    )
+
+    return (splashScreenActif ? splashScreen : this.props.children)
+  }
+}
+
+export default SplashScreen
