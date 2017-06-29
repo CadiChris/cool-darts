271de11035188d478e6108100544637ce6e5b489
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-06-29 15:29:38 +0200

    [Refacto] Éclatement en 1 container + 1 vue pure.

---
+++ app-react-native/src/Cricket/CommandesDeLaPartie/CommandesDeLaPartie.container.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { nouvellePartie } from './../Cricket.actions'
+import CommandesDeLaPartie from './CommandesDeLaPartie'
+
+class CommandesDeLaPartieContainer extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <CommandesDeLaPartie {...this.props} />
+  }
+}
+
+const mapStateToProps = (state) => ({
+  aucunPrecedent: state.cricket.precedents[state.cricket.precedents.length-1].phase === 'INSCRIPTION'
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  declencherNouvellePartie: () => dispatch(nouvellePartie()),
+  declencherUndo: () => dispatch({type:'UNDO'})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommandesDeLaPartieContainer)
--- app-react-native/src/Cricket/CommandesDeLaPartie/CommandesDeLaPartie.js
+++ app-react-native/src/Cricket/CommandesDeLaPartie/CommandesDeLaPartie.js
@@ -1,51 +1,25 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Text, View }  from 'react-native'
 import Button from 'apsl-react-native-button'
 import { Boutons, Textes } from '../styles'
-import { nouvellePartie } from './../Cricket.actions'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
-class CommandesDeLaPartie extends React.Component {
+export default ({aucunPrecedent : undoImpossible, declencherNouvellePartie, declencherUndo}) => (
 
-  constructor(props) {
-    super(props)
-  }
+  <View style={[{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 10}]}>
+    <Button
+      onPress={declencherNouvellePartie}
+      style={[Boutons.deCommande, {width: 60, height: 50, marginHorizontal: 30}]}
+      textStyle={[Textes.bouton, {fontSize: 12}]}>
+      <Icon name="home" size={20} color="white"/>
+    </Button>
+    <Button
+      onPress={declencherUndo}
+      isDisabled={undoImpossible}
+      style={[Boutons.deCommande, {width: 60, height: 50, marginHorizontal: 30}]}
+      textStyle={[Textes.bouton, {fontSize: 12}]}>
+      <Icon name="undo" size={20} color="white"/>
+    </Button>
+  </View>
 
-  render() {
-    const {aucunPrecedent} = this.props
-    return (
-      <View style={[{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 10}]}>
-        <Button
-          onPress={() => this.nouvellePartie()}
-          style={[Boutons.deCommande, {width: 60, height: 50, marginHorizontal: 30}]}
-          textStyle={[Textes.bouton, {fontSize: 12}]}>
-          <Icon name="home" size={20} color="white"/>
-        </Button>
-        <Button
-          onPress={() => this.undo()}
-          isDisabled={aucunPrecedent}
-          style={[Boutons.deCommande, {width: 60, height: 50, marginHorizontal: 30}]}
-          textStyle={[Textes.bouton, {fontSize: 12}]}>
-          <Icon name="undo" size={20} color="white"/>
-        </Button>
-      </View>
-    )
-  }
-
-  nouvellePartie() {
-    const {dispatch} = this.props
-    dispatch(nouvellePartie())
-  }
-
-  undo() {
-    const {dispatch} = this.props
-    dispatch({type:'UNDO'})
-  }
-}
-
-const mapStateToProps = (state) => ({
-  aucunPrecedent: state.cricket.precedents[state.cricket.precedents.length-1].phase === 'INSCRIPTION'
-})
-
-export default connect(mapStateToProps)(CommandesDeLaPartie)
+)
