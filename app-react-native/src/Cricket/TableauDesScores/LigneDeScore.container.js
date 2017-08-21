0c2fb159a49f6dda6227d7c5e8020ecae424c8e4
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-08-21 11:06:46 +0200

    Vibration lors du toucher du tableau.

--- app-react-native/src/Cricket/TableauDesScores/LigneDeScore.container.js
+++ app-react-native/src/Cricket/TableauDesScores/LigneDeScore.container.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { lancerFlechette } from '../Cricket.actions'
 import LigneDeScore from './LigneDeScore'
+import { Vibration } from "react-native"
 
 class LigneDeScoreContainer extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class LigneDeScoreContainer extends React.Component {
   lancerSimpleDans(chiffre) {
     const {dispatch, score} = this.props
     dispatch(lancerFlechette(score.joueur, chiffre, 1))
+    Vibration.vibrate(50)
   }
 }
 
