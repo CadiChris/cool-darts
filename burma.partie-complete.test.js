984cfded37d714d4f492b6299cc4d372e22141a8
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-10-29 19:26:35 +0100

    Test de partie complète : jusqu'au 16.

--- burma.partie-complete.test.js
+++ burma.partie-complete.test.js
@@ -6,9 +6,15 @@ it("déroule une partie complète", () => {
   const burma2joueurs = burma(burma1joueur, inscrireJoueur("J2"));
   const burmaEnCours = burma(burma2joueurs, demarrerPartie());
 
-  const v_15_j1 = burma(burmaEnCours, volee("J1", 15, 0));
-  expect(v_15_j1).toMatchSnapshot();
+  const volee_15_j1 = burma(burmaEnCours, volee("J1", 15, 0));
+  expect(volee_15_j1).toMatchSnapshot();
 
-  const v_15_j2 = burma(v_15_j1, volee("J2", 15, 2));
-  expect(v_15_j2).toMatchSnapshot();
+  const volee_15_j2 = burma(volee_15_j1, volee("J2", 15, 2));
+  expect(volee_15_j2).toMatchSnapshot();
+
+  const v16_j1 = burma(volee_15_j2, volee("J1", 16, 1));
+  expect(v16_j1).toMatchSnapshot();
+
+  const v16_j2 = burma(v16_j1, volee("J2", 16, 4));
+  expect(v16_j2).toMatchSnapshot();
 });
