270d7e3702767e4e313733f3ad11cab0bb422574
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-11-05 14:31:00 +0100

    Déroulement de la partie complète.

--- burma.partie-complete.test.js
+++ burma.partie-complete.test.js
@@ -3,7 +3,8 @@ import {
   inscrireJoueur,
   demarrerPartie,
   voleeChiffree,
-  voleeSurDouble
+  voleeSurDouble,
+  voleeSurTriple
 } from "./burma.actions";
 
 it("déroule une partie complète", () => {
@@ -17,9 +18,9 @@ it("déroule une partie complète", () => {
 // 15   0     -> /2   ->  20 pts      2 -> +30  -> 70 pts
 // 16   1     -> +16  ->  36 pts      4 -> +64  -> 134 pts
 // D    2x20  -> +40  ->  76 pts      0 -> /2   -> 67 pts
-// 17
-// 18
-// T
+// 17   0     -> /2   ->  38 pts      2 -> +34  -> 101 pts
+// 18   3     -> +54  ->  92 pts      1 -> +18  -> 119 pts
+// T    3x20  -> +60  ->  152 pts     0 -> /2   -> 60 pts
 // 19
 // 20
 // B
@@ -30,7 +31,13 @@ const partie = [
   voleeChiffree("J1", 16, 1),
   voleeChiffree("J2", 16, 4),
   voleeSurDouble("J1", [{ chiffre: 20, nombre: 2 }]),
-  voleeSurDouble("J2", [])
+  voleeSurDouble("J2", []),
+  voleeChiffree("J1", 17, 0),
+  voleeChiffree("J2", 17, 2),
+  voleeChiffree("J1", 18, 3),
+  voleeChiffree("J2", 18, 1),
+  voleeSurTriple("J1", [{ chiffre: 20, nombre: 3 }]),
+  voleeSurTriple("J2", [])
 ];
 
 const derouler = partie => ({
