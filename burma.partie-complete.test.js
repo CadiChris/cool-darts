80ad2fef3602f53bf12ef0065a6d72d98a8aff65
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-11-05 14:38:39 +0100

    Déroulement de la partie complète.

--- burma.partie-complete.test.js
+++ burma.partie-complete.test.js
@@ -15,14 +15,14 @@ it("déroule une partie complète", () => {
   derouler(partie).depuis(burmaEnCours);
 });
 
-// 15   0     -> /2   ->  20 pts      2 -> +30  -> 70 pts
-// 16   1     -> +16  ->  36 pts      4 -> +64  -> 134 pts
-// D    2x20  -> +40  ->  76 pts      0 -> /2   -> 67 pts
-// 17   0     -> /2   ->  38 pts      2 -> +34  -> 101 pts
-// 18   3     -> +54  ->  92 pts      1 -> +18  -> 119 pts
-// T    3x20  -> +60  ->  152 pts     0 -> /2   -> 60 pts
-// 19
-// 20
+// 15   0     -> /2   -> 20 pts      2 -> +30  -> 70 pts
+// 16   1     -> +16  -> 36 pts      4 -> +64  -> 134 pts
+// D    2x20  -> +40  -> 76 pts      0 -> /2   -> 67 pts
+// 17   0     -> /2   -> 38 pts      2 -> +34  -> 101 pts
+// 18   3     -> +54  -> 92 pts      1 -> +18  -> 119 pts
+// T    3x20  -> +60  -> 152 pts     0 -> /2   -> 60 pts
+// 19   0     -> /2   -> 76 pts      2 -> 38   -> 98 pts
+// 20   2     -> +40  -> 116 pts     1 -> +20  -> 118 pts
 // B
 
 const partie = [
@@ -37,7 +37,11 @@ const partie = [
   voleeChiffree("J1", 18, 3),
   voleeChiffree("J2", 18, 1),
   voleeSurTriple("J1", [{ chiffre: 20, nombre: 3 }]),
-  voleeSurTriple("J2", [])
+  voleeSurTriple("J2", []),
+  voleeChiffree("J1", 19, 0),
+  voleeChiffree("J2", 19, 2),
+  voleeChiffree("J1", 20, 2),
+  voleeChiffree("J2", 20, 1)
 ];
 
 const derouler = partie => ({
