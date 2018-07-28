6f42b9b1ce001a87e750169c317289068f3cfe93
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-07-28 20:26:30 +0200

    [Boyscout] Regroupe plus clairement les tests par fonctionnalité.
    Pas de changement dans les tests par ce commit. Simplement du rangement pour mieux comprendre.

--- app-react-native/src/Cricket/arbitrage/vainqueurs.test.js
+++ app-react-native/src/Cricket/arbitrage/vainqueurs.test.js
@@ -3,35 +3,46 @@ import { score } from "./arbitre.test";
 
 const _3_TOUCHES = 3;
 
-it("pas de vainqueurs si certains chiffres sont ouverts", () => {
-  const seulementLe20Ferme = score("J1", { 20: _3_TOUCHES }, 0);
-  expect(vainqueurs([seulementLe20Ferme])).toEqual([]);
+describe("Choix du vainqueur", () => {
+  describe("Aucune cible férmee", () => {
+    it("ne donne pas de vainqueur si certains chiffres sont ouverts", () => {
+      const seulementLe20Ferme = score("J1", { 20: _3_TOUCHES }, 0);
+      const pasDeVainqueur = [];
+
+      expect(vainqueurs([seulementLe20Ferme])).toEqual(pasDeVainqueur);
+    });
+  });
+
+  describe("Une seule cible fermée", () => {
+    it("déclare vainqueur celui qui a tous ses chiffres fermés", () => {
+      const seulementLe20Ferme = score("J1", { 20: _3_TOUCHES }, 0);
+      const toutFerme = scoreAvecCibleFermee("J2");
+
+      expect(vainqueurs([seulementLe20Ferme, toutFerme])).toEqual(["J2"]);
+    });
+  });
+  describe("Plusieurs cibles fermées", () => {
+    it("déclare le joueur avec le moins de pénalité comme vainqueur", () => {
+      expect(
+        vainqueurs([
+          scoreAvecCibleFermee("joueur avec pénalité", 150),
+          scoreAvecCibleFermee("joueur sans pénalité", 0)
+        ])
+      ).toEqual(["joueur sans pénalité"]);
+    });
+    it("déclare plusieurs vainqueurs si toutes les cibles fermées ont la même pénalité", () => {
+      expect(
+        vainqueurs([
+          scoreAvecCibleFermee("J1", 30),
+          scoreAvecCibleFermee("J2", 30)
+        ])
+      ).toEqual(["J1", "J2"]);
+    });
+  });
 });
 
-it("déclare vainqueur celui qui a tous ses chiffres fermés", () => {
-  const seulementLe20Ferme = score("J1", { 20: _3_TOUCHES }, 0);
-  const toutFerme = scoreAvecCibleFermee("J2");
-
-  expect(vainqueurs([seulementLe20Ferme, toutFerme])).toEqual(["J2"]);
-});
-
-it("déclare le joueur avec le moins de pénalité comme vainqueur", () => {
-  expect(
-    vainqueurs([
-      scoreAvecCibleFermee("joueur avec pénalité", 150),
-      scoreAvecCibleFermee("joueur sans pénalité", 0)
-    ])
-  ).toEqual(["joueur sans pénalité"]);
-});
-
-it("déclare plusieurs vainqueurs en cas d'égalité", () => {
-  expect(
-    vainqueurs([scoreAvecCibleFermee("J1", 30), scoreAvecCibleFermee("J2", 30)])
-  ).toEqual(["J1", "J2"]);
-});
-
-const scoreAvecCibleFermee = function(leJoueur, saPenalite = 0) {
-  return score(
+const scoreAvecCibleFermee = (leJoueur, saPenalite = 0) =>
+  score(
     leJoueur,
     {
       20: _3_TOUCHES,
@@ -44,4 +55,3 @@ const scoreAvecCibleFermee = function(leJoueur, saPenalite = 0) {
     },
     saPenalite
   );
-};
