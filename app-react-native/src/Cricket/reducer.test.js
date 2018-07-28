73a987e3ec47d688f21fb2d76b5b7e474e31620c
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-07-28 21:35:40 +0200

    [Boyscout] Supprime les "toMatchSnapshot()" des assertions, au profit d'assertions explicites.
    C'est beaucoup plus efficace d'avoir des assertions explicites plutôt que d'obscurs toMatchSnapshot() qui n'expriment pas grand-chose.

--- app-react-native/src/Cricket/reducer.test.js
+++ app-react-native/src/Cricket/reducer.test.js
@@ -1,3 +1,4 @@
+import freeze from "deep-freeze";
 import cricket from "./reducer";
 import {
   inscrireJoueur,
@@ -7,44 +8,104 @@ import {
 } from "./actions";
 
 it("retourne le state initial", () => {
-  expect(cricket(undefined, {})).toMatchSnapshot();
+  expect(cricket(undefined, {})).toEqual({
+    joueurs: [],
+    peutDemarrer: false,
+    phase: "INSCRIPTION",
+    scores: [],
+    vainqueurs: []
+  });
 });
 
 it("inscrit des joueurs", () => {
-  expect(partieAvec1Joueur("J1")).toMatchSnapshot();
-  expect(
-    cricket(partieAvec1Joueur("J1"), inscrireJoueur("J2"))
-  ).toMatchSnapshot();
+  const avecJoueurs = executer([inscrireJoueur("J1"), inscrireJoueur("J2")]);
+
+  expect(avecJoueurs.peutDemarrer).toBe(true);
+  expect(avecJoueurs.joueurs).toEqual(["J1", "J2"]);
+  expect(avecJoueurs.scores).toEqual([
+    {
+      joueur: "J1",
+      points: 0,
+      cible: {
+        15: { touches: 0, ferme: false },
+        16: { touches: 0, ferme: false },
+        17: { touches: 0, ferme: false },
+        18: { touches: 0, ferme: false },
+        19: { touches: 0, ferme: false },
+        20: { touches: 0, ferme: false },
+        25: { touches: 0, ferme: false }
+      }
+    },
+    {
+      joueur: "J2",
+      points: 0,
+      cible: {
+        15: { touches: 0, ferme: false },
+        16: { touches: 0, ferme: false },
+        17: { touches: 0, ferme: false },
+        18: { touches: 0, ferme: false },
+        19: { touches: 0, ferme: false },
+        20: { touches: 0, ferme: false },
+        25: { touches: 0, ferme: false }
+      }
+    }
+  ]);
 });
 
 it("démarre la partie", () => {
-  expect(cricket(partieAvec1Joueur("J1"), demarrerPartie())).toMatchSnapshot();
+  const partieDemarree = executer([
+    inscrireJoueur("J1"),
+    inscrireJoueur("J2"),
+    demarrerPartie()
+  ]);
+
+  expect(partieDemarree.phase).toBe("EN_COURS");
 });
 
 it("modifie le score sur un lancer de fléchette", () => {
-  expect(
-    cricket(partieAvec1Joueur("J1"), lancerFlechette("J1", 20, 1))
-  ).toMatchSnapshot();
+  const apresLancerDeJ1 = executer([
+    inscrireJoueur("J1"),
+    lancerFlechette("J1", 20, 1)
+  ]);
+
+  expect(apresLancerDeJ1.scores[0].cible[20]).toEqual({
+    touches: 1,
+    ferme: false
+  });
 });
 
 it("met fin à la partie sur le lancer qui désigne le vainqueur", () => {
+  const partieAvecUnJoueur = executer([inscrireJoueur("J1"), demarrerPartie()]);
+
   const toutFermeSaufLeBull = [15, 16, 17, 18, 19, 20].reduce(
-    (tableau, chiffre) => cricket(tableau, lancerFlechette("J1", chiffre, 3)),
-    partieAvec1Joueur("J1")
+    (state, chiffre) => cricket(state, lancerFlechette("J1", chiffre, 3)),
+    partieAvecUnJoueur
   );
+
+  expect(toutFermeSaufLeBull.phase).toBe("EN_COURS");
 
   const fermerLeBull = lancerFlechette("J1", 25, 3);
   const partieTerminee = cricket(toutFermeSaufLeBull, fermerLeBull);
 
-  expect(partieTerminee.vainqueurs).toEqual(["J1"]);
   expect(partieTerminee.phase).toEqual("TERMINEE");
+  expect(partieTerminee.vainqueurs).toEqual(["J1"]);
 });
 
 it("recrée une nouvelle partie", () => {
-  const partieDemarree = cricket(partieAvec1Joueur("J1"), demarrerPartie());
-  expect(cricket(partieDemarree, nouvellePartie())).toMatchSnapshot();
+  const partieVierge = cricket(undefined, {});
+
+  const partieRedemarree = executer([
+    inscrireJoueur("J1"),
+    demarrerPartie(),
+    nouvellePartie()
+  ]);
+
+  expect(partieRedemarree).toEqual(partieVierge);
 });
 
-const partieAvec1Joueur = joueur =>
-  cricket(partieVide(), inscrireJoueur(joueur));
-const partieVide = () => cricket(undefined, {});
+const executer = actions =>
+  actions.reduce((state, action) => {
+    const nextState = cricket(state, action);
+    freeze(nextState);
+    return nextState;
+  }, undefined);
