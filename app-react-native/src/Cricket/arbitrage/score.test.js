7a93a1254a171fef4154be82449b63ecc666f583
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-07-28 21:12:02 +0200

    [Boyscout] Supprime les "toMatchSnapshot()" des assertions, au profit d'assertions explicites.
    C'est beaucoup plus efficace d'avoir des assertions explicites plutôt que d'obscurs toMatchSnapshot() qui n'expriment pas grand-chose.

--- app-react-native/src/Cricket/arbitrage/score.test.js
+++ app-react-native/src/Cricket/arbitrage/score.test.js
@@ -1,12 +1,23 @@
-import assert from "assert";
 import { scoreVierge } from "./score";
 
 describe("score.modele", () => {
   it("crée un score vierge", () => {
-    expect(scoreVierge("J1")).toMatchSnapshot();
+    expect(scoreVierge("J1")).toEqual({
+      joueur: "J1",
+      points: 0,
+      cible: {
+        15: { touches: 0, ferme: false },
+        16: { touches: 0, ferme: false },
+        17: { touches: 0, ferme: false },
+        18: { touches: 0, ferme: false },
+        19: { touches: 0, ferme: false },
+        20: { touches: 0, ferme: false },
+        25: { touches: 0, ferme: false }
+      }
+    });
   });
 
   it("créé un nouveau score à chaque appel", () => {
-    assert.notStrictEqual(scoreVierge(""), scoreVierge(""));
+    expect(scoreVierge("")).not.toBe(scoreVierge(""));
   });
 });
