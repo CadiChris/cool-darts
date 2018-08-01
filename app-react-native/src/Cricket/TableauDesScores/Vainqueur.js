3dcd8f91346a00ceca34a9ac43f93d43928ddf35
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-08-01 22:47:15 +0200

    Upgrade react-native en 0.51.0
    J'update les packages qui en ont besoin.
    cf. https://github.com/react-community/create-react-native-app/blob/master/VERSIONS.md
    emoji : This is a fork off of jorilallo/react-native-emoji because of issue #19

--- app-react-native/src/Cricket/TableauDesScores/Vainqueur.js
+++ app-react-native/src/Cricket/TableauDesScores/Vainqueur.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Text } from "react-native";
-import Emoji from "react-native-emoji";
+import Emoji from "@ardentlabs/react-native-emoji";
 import { Textes } from "../styles";
 import TexteApparaissant from "../Technique/TexteApparaissant";
 
