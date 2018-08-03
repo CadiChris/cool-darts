65d700fe5096af12e8bcc98060e5c74fd5e8b430
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-08-04 01:23:33 +0200

    Pas de bordure entre les contrats dans la <ColonneDesContrats />

--- app-react-native/src/Burma/TableauDesScores/ColonneDesContrats.js
+++ app-react-native/src/Burma/TableauDesScores/ColonneDesContrats.js
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import { Styles, Textes } from "../../styles";
+import { Textes } from "../../styles";
 import { CHIFFRES_DU_BURMA } from "../arbitrage/chiffre";
 import PropTypes from "prop-types";
 import React from "react";
@@ -10,16 +10,11 @@ import {
 
 export const ColonneDesContrats = () => (
   <View style={{ width: 90, marginTop: 40 }}>
-    <View
-      style={[{ height: HAUTEUR_DU_CONTRAT_DE_DEPART }, Styles.bordureBasse]}
-    >
+    <View style={[{ height: HAUTEUR_DU_CONTRAT_DE_DEPART }]}>
       <View style={{ flex: 1, justifyContent: "center" }} />
     </View>
     {CHIFFRES_DU_BURMA.map(contrat => (
-      <View
-        key={contrat}
-        style={[{ height: HAUTEUR_DE_CONTRAT }, Styles.bordureBasse]}
-      >
+      <View key={contrat} style={[{ height: HAUTEUR_DE_CONTRAT }]}>
         <View style={{ flex: 1, paddingLeft: 5, justifyContent: "center" }}>
           <Text style={[Textes.light, { fontSize: 18 }]}>{contrat}</Text>
         </View>
