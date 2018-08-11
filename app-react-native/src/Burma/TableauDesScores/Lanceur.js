a6764d7f4fa39f09e0c7080b31a09c551aee5120
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-08-11 13:23:48 +0200

    Enlève le rose derrière la cible, qui était trop distrayant.

--- app-react-native/src/Burma/TableauDesScores/Lanceur.js
+++ app-react-native/src/Burma/TableauDesScores/Lanceur.js
@@ -2,7 +2,6 @@ import React from "react";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { View } from "react-native";
 import { HAUTEUR_DE_CONTRAT } from "./TableauDesScores";
-import { Couleurs } from "../../styles";
 import FadeInView from "../../Technique/FadeInView";
 
 export const Lanceur = () => (
@@ -10,11 +9,8 @@ export const Lanceur = () => (
     <View
       style={{
         flex: 1,
-        margin: 6,
         justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: Couleurs.deux,
-        borderRadius: 4
+        alignItems: "center"
       }}
     >
       <Icon name="target" color="white" size={20} style={{ opacity: 0.85 }} />
