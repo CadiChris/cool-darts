daec5b9bc9c2fadfc894032a7f6f6cc6b8f43362
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-08-11 15:56:48 +0200

    Conversion en composant fonctionnel.

--- app-react-native/src/Burma/CommandesDeLaPartie/CommandesDeLaPartie.js
+++ app-react-native/src/Burma/CommandesDeLaPartie/CommandesDeLaPartie.js
@@ -1,31 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import ContratChiffre from "./Contrats/ContratChiffre";
 import ContratDouble from "./Contrats/ContratDouble";
 
-class CommandesDeLaPartie extends Component {
-  render() {
-    const {
-      chiffreCourant,
-      lanceur,
-      onLancerSurChiffre,
-      onLancerSurDouble
-    } = this.props;
-
-    switch (chiffreCourant) {
-      case "DOUBLE":
-        return <ContratDouble lanceur={lanceur} onLancer={onLancerSurDouble} />;
-      default:
-        return (
-          <ContratChiffre
-            chiffreCourant={chiffreCourant}
-            lanceur={lanceur}
-            onLancer={onLancerSurChiffre}
-          />
-        );
-    }
+const CommandesDeLaPartie = ({
+  chiffreCourant,
+  lanceur,
+  onLancerSurChiffre,
+  onLancerSurDouble
+}) => {
+  switch (chiffreCourant) {
+    case "DOUBLE":
+      return <ContratDouble lanceur={lanceur} onLancer={onLancerSurDouble} />;
+    default:
+      return (
+        <ContratChiffre
+          chiffreCourant={chiffreCourant}
+          lanceur={lanceur}
+          onLancer={onLancerSurChiffre}
+        />
+      );
   }
-}
+};
 
 CommandesDeLaPartie.propTypes = {
   lanceur: PropTypes.string,
