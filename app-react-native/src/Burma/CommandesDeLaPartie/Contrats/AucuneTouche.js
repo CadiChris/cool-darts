fbeccd485692dfd329ec20e580a49b522bb4644e
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-08-14 19:40:52 +0200

    Localisation du composant.

--- app-react-native/src/Burma/CommandesDeLaPartie/Contrats/AucuneTouche.js
+++ app-react-native/src/Burma/CommandesDeLaPartie/Contrats/AucuneTouche.js
@@ -1,9 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
 import { Text, View } from "react-native";
 import Button from "apsl-react-native-button";
-import { Boutons, Textes } from "../../../styles";
 import Icon from "react-native-vector-icons/FontAwesome";
-import PropTypes from "prop-types";
-import React from "react";
+import LocalizedStrings from "react-native-localization";
+import { Boutons, Textes } from "../../../styles";
 
 const AucuneTouche = props => (
   <View>
@@ -12,10 +13,19 @@ const AucuneTouche = props => (
       onPress={props.onPress}
     >
       <Icon name="ban" size={20} color="white" style={{ marginRight: 10 }} />
-      <Text style={[Textes.light, { fontSize: 20 }]}>Aucun</Text>
+      <Text style={[Textes.light, { fontSize: 20 }]}>{textes.aucun}</Text>
     </Button>
   </View>
 );
+
+const textes = new LocalizedStrings({
+  en: {
+    aucun: "None"
+  },
+  fr: {
+    aucun: "Aucun"
+  }
+});
 
 AucuneTouche.propTypes = { onPress: PropTypes.func };
 
