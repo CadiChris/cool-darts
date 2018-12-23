1b1df7aa5b5474233653484d953fb184b367f4d5
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2018-12-23 18:27:59 +0100

    Extrait la colonne de joueur dans son composant

---
+++ app-react-native/src/Cricket/TableauDesScores/ColonneJoueur.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Text, TouchableHighlight, View } from "react-native";
+import { HAUTEUR_DE_LIGNE } from "./dimensions";
+import Joueur from "./Joueur";
+import { FontSizes, Styles, Textes } from "../../styles";
+import Touches from "./Touches";
+import AnimatedNumber from "../../Technique/AnimatedNumber";
+
+export const ColonneJoueur = function({
+  score,
+  style,
+  actif,
+  onLancerDansSimple
+}) {
+  return (
+    <View style={style}>
+      <View style={{ height: HAUTEUR_DE_LIGNE }}>
+        <Joueur nom={score.joueur} />
+      </View>
+      {[20, 19, 18, 17, 16, 15, 25].map(chiffre => (
+        <TouchableHighlight
+          key={chiffre}
+          disabled={!actif}
+          onPress={() => onLancerDansSimple(score.joueur, chiffre)}
+        >
+          <View
+            style={[
+              { height: HAUTEUR_DE_LIGNE, justifyContent: "center" },
+              Styles.bordureBasse,
+              score.cible[chiffre].ferme && Styles.chiffreFerme
+            ]}
+          >
+            <View>
+              <Touches symbole="X" nombre={score.cible[chiffre].touches} />
+            </View>
+          </View>
+        </TouchableHighlight>
+      ))}
+      <View
+        style={[
+          Styles.bordureHaute,
+          {
+            height: HAUTEUR_DE_LIGNE,
+            borderTopWidth: 3,
+            justifyContent: "center"
+          }
+        ]}
+      >
+        <AnimatedNumber value={score.penalite}>
+          {value => (
+            <Text
+              style={[
+                Textes.titre,
+                { fontSize: FontSizes.standard, textAlign: "center" }
+              ]}
+            >
+              {Math.round(value)}
+            </Text>
+          )}
+        </AnimatedNumber>
+      </View>
+    </View>
+  );
+};
+
+ColonneJoueur.propTypes = {
+  score: PropTypes.object.isRequired,
+  style: PropTypes.array.isRequired,
+  actif: PropTypes.bool.isRequired,
+  onLancerDansSimple: PropTypes.func.isRequired
+};
--- app-react-native/src/Cricket/TableauDesScores/TableauDesScores.js
+++ app-react-native/src/Cricket/TableauDesScores/TableauDesScores.js
@@ -1,16 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import * as Animatable from "react-native-animatable";
-import { Text, TouchableHighlight, View } from "react-native";
+import { View } from "react-native";
 import Celebration from "../../Technique/Celebration";
-import { FontSizes, Styles, Textes } from "../../styles";
-import Joueur from "./Joueur";
-import Touches from "./Touches";
-import AnimatedNumber from "../../Technique/AnimatedNumber";
+import { Styles } from "../../styles";
 import Vainqueurs from "./Vainqueurs";
 import { splitArray } from "../../utils/splitArray";
 import { ColonneDesChiffres } from "./ColonneDesChiffres";
-import { HAUTEUR_DE_LIGNE, LARGEUR_COLONNE_JOUEUR } from "./dimensions";
+import { LARGEUR_COLONNE_JOUEUR } from "./dimensions";
+import { ColonneJoueur } from "./ColonneJoueur";
 
 const TableauDesScores = ({
   scores,
@@ -21,92 +19,42 @@ const TableauDesScores = ({
   <Animatable.View style={[{ flex: 1 }]} animation="bounceInRight">
     {vainqueurs.length > 0 && <Celebration />}
     <View style={{ flexDirection: "row" }}>
-      {splitArray(scores).premier.map(score =>
-        colonneJoueur(
-          score,
-          [
+      {splitArray(scores).premier.map(score => (
+        <ColonneJoueur
+          key={score.joueur}
+          score={score}
+          style={[
             {
               width: LARGEUR_COLONNE_JOUEUR(scores.length)
             },
             Styles.bordureDroite
-          ],
-          actif,
-          onLancerDansSimple
-        )
-      )}
+          ]}
+          actif={actif}
+          onLancerDansSimple={onLancerDansSimple}
+        />
+      ))}
 
       {ColonneDesChiffres}
 
-      {splitArray(scores).second.map(score =>
-        colonneJoueur(
-          score,
-          [
+      {splitArray(scores).second.map(score => (
+        <ColonneJoueur
+          key={score.joueur}
+          score={score}
+          style={[
             {
               width: LARGEUR_COLONNE_JOUEUR(scores.length)
             },
             Styles.bordureGauche
-          ],
-          actif,
-          onLancerDansSimple
-        )
-      )}
+          ]}
+          actif={actif}
+          onLancerDansSimple={onLancerDansSimple}
+        />
+      ))}
     </View>
 
     {vainqueurs.length > 0 && <Vainqueurs noms={vainqueurs} />}
   </Animatable.View>
 );
-
-const colonneJoueur = function(score, style, actif, onLancerDansSimple) {
-  return (
-    <View key={score.joueur} style={style}>
-      <View style={{ height: HAUTEUR_DE_LIGNE }}>
-        <Joueur nom={score.joueur} />
-      </View>
-      {[20, 19, 18, 17, 16, 15, 25].map(chiffre => (
-        <TouchableHighlight
-          key={chiffre}
-          disabled={!actif}
-          onPress={() => onLancerDansSimple(score.joueur, chiffre)}
-        >
-          <View
-            style={[
-              { height: HAUTEUR_DE_LIGNE, justifyContent: "center" },
-              Styles.bordureBasse,
-              score.cible[chiffre].ferme && Styles.chiffreFerme
-            ]}
-          >
-            <View>
-              <Touches symbole="X" nombre={score.cible[chiffre].touches} />
-            </View>
-          </View>
-        </TouchableHighlight>
-      ))}
-      <View
-        style={[
-          Styles.bordureHaute,
-          {
-            height: HAUTEUR_DE_LIGNE,
-            borderTopWidth: 3,
-            justifyContent: "center"
-          }
-        ]}
-      >
-        <AnimatedNumber value={score.penalite}>
-          {value => (
-            <Text
-              style={[
-                Textes.titre,
-                { fontSize: FontSizes.standard, textAlign: "center" }
-              ]}
-            >
-              {Math.round(value)}
-            </Text>
-          )}
-        </AnimatedNumber>
-      </View>
-    </View>
-  );
-};
 
 TableauDesScores.propTypes = {
   scores: PropTypes.array.isRequired,
