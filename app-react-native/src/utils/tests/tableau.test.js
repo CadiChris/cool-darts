bdc5fa6d848c097459f8474c05b0cbe0f784ebfd
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2020-05-10 11:59:34 +0200

    Réorganiser des tests

--- app-react-native/src/utils/tests/tableau.test.js
+++ app-react-native/src/utils/tests/tableau.test.js
@@ -1,6 +1,6 @@
 import {elementPrecedent, elementSuivant, split} from '../tableau';
 
-describe('tableau', () => {
+describe('élément suivant', () => {
   const joueurs = ['J1', 'J2'];
 
   it("retourne l'élément suivant en revenant au début après la fin", () => {
@@ -12,27 +12,28 @@ describe('tableau', () => {
     expect(elementPrecedent('J2', joueurs)).toBe('J1');
     expect(elementPrecedent('J1', joueurs)).toBe('J2');
   });
-  describe('split', () => {
-    it('split au milieu un array de longueur paire', () => {
-      const deLongueurPaire = [1, 2, 3, 4];
+});
 
-      const morceaux = split(deLongueurPaire);
+describe('split', () => {
+  it('split au milieu un array de longueur paire', () => {
+    const deLongueurPaire = [1, 2, 3, 4];
 
-      expect(morceaux).toEqual({
-        premier: [1, 2],
-        second: [3, 4],
-      });
+    const morceaux = split(deLongueurPaire);
+
+    expect(morceaux).toEqual({
+      premier: [1, 2],
+      second: [3, 4],
     });
+  });
 
-    it('split après le milieu un array de longueur impaire', () => {
-      const deLongueurImpaire = [1, 2, 3];
+  it('split après le milieu un array de longueur impaire', () => {
+    const deLongueurImpaire = [1, 2, 3];
 
-      const morceaux = split(deLongueurImpaire);
+    const morceaux = split(deLongueurImpaire);
 
-      expect(morceaux).toEqual({
-        premier: [1],
-        second: [2, 3],
-      });
+    expect(morceaux).toEqual({
+      premier: [1],
+      second: [2, 3],
     });
   });
 });
